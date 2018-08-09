02602ed12794ac172b36f00e8b38ca8d1da5599f
roylee1314 <roy__lee@163.com>
2018-08-09 18:01:02 +0800

    bj ai rule

--- assets/Script/bj-rule.js
+++ assets/Script/bj-rule.js
@@ -118,15 +118,90 @@ function bjAI(nineCards){
         if (c0.point != c0.point){
             return c0.point - c1.point;
         }
-        // for trip, suit does not matter
-        // we use the smaller suit
-        return c1.suit - c0.suit;
+        
+        return c0.suit - c1.suit;
     });
+
+    var pos = 0;
+
+    points = new Array(14).fill({});
+    suits = new Array(5).fill({});
+    for (var i = 0; i < nineCards.length; i++) {
+        points[nineCards[i].point].add(i);
+        suits[nineCards[i].suit].add(i);
+    };
+
+    // trip
+    for (var i = points.length - 1; i >= 0; i--) {
+        if (points[i].length > 3){
+            for(var j in points[i]){
+                card = nineCards[points[i][j]];
+                if (suits[card.suit].length == 3){
+                    point[i].delete(points[i][j]);
+                    break;
+                }
+            }
+        }
+        if (points[i].length == 3){
+            for(var j in points[i]){
+                card = nineCards[points[i][j]];
+                cards[pos++] = card;
+                suits[card.suit].delete(points[i][j]); 
+            }
+        }
+
+    };
     var i = 0;
-    while(i < nineCards.length)
-    {
+    while (i < suits.length) {
+        if (suits[i].length > 3){
+            array = {}
+            for(var j in suits[i]){
+                card = nineCards[suits[i][j]];
+                array.push(card);
+            }
+           
+            for(var j = 13; j >= 0; j--){
+                for(var k in )
+            }
+        }
+
+
+    }
+
+
+
+    // find trip
+    // var i = 0;
+    // while(i < nineCards.length)
+    // {
+    //     if(nineCards[i] == null){
+    //         i++;
+    //         continue;
+    //     }
+    //     var card = nineCards[i];
+    //     var count = 0;
+    //     j = i + 1;
+    //     while (j < nineCards.length){
+    //         if (nineCards[j] != null && nineCards[j].point == card.point){
+    //             j ++;
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //     if (count >= 3){
+    //         for (c = 0; c < 3; c++){
+    //             cards[pos++] = nineCards[i];
+    //             nineCards[i++] = null;
+    //         }
             
-    }    
+    //     }else{
+    //         i = j;
+    //     }
+
+    // }
+
+    //find
 
 
 }
